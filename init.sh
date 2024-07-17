ebdb96d67ef7726599b85197dedfb6e90dd847a9
Kyle <kylejones199108@gmail.com>
2024-07-17 00:06:02 -0400

    Okay fixing this...not sure why this is an issue on my end

--- init.sh
+++ init.sh
@@ -9,6 +9,8 @@ install_helm() {
         curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
         chmod 700 get_helm.sh
         ./get_helm.sh
+        echo 'export PATH=$PATH:/usr/local/bin' >> ~/.bashrc
+        source ~/.bashrc
     fi
 }
 
